bfc2992517bf2786e0849c69320791202abcc6c5
dimanvaz <dimanvaz04@gmail.com>
2022-03-19 00:57:01 +0300

    remove file save\parse - only read to memory now

--- jsonHandler.js
+++ jsonHandler.js
@@ -43,7 +43,7 @@ function personal(author){
         });
     names_posts[author].allWords = Object.fromEntries(ascen);
     console.log(author);
-    //console.table(ascen.slice(0,30)); // красивая табличка на каждого человека
+    console.table(ascen.slice(0,30)); // красивая табличка на каждого человека
     //console.log(author,'\n',names_posts[author]);
 }
 
@@ -61,16 +61,62 @@ exP.get('/', (request,response)=>{
 })
 
 exP.post('/sendFile',(request,response)=>{
-
+    chatArray = [];
+    names_posts = {};
     let ch = '';
     request.on('data',chunk=> ch+=chunk);
     request.on('end', ()=>{
             console.log('1: received full file data');
 
-        let entrary = ch.slice(144,ch.length-44); //обрезанное без лишних вебкит-строк
-        fs.writeFileSync('./uploads/out.json',entrary);
-            console.log('2: file wrote to disk');
+        let cuttedString = ch.slice(144,ch.length-44); //обрезанное без лишних вебкит-строк
+        chatArray = JSON.parse(cuttedString).messages;
 
+            if(chatArray.length > 0){
+                console.log('2: chat array filled,length:', chatArray.length);
+            } else return
+
+        //заполнение объекта именами и счётчиками сообщений
+        chatArray.forEach(message => {
+
+            if(message.from !== undefined){
+                names_posts[message.from]?
+                    names_posts[message.from].total++ :
+                    names_posts[message.from]={total:1};
+
+                if(message.text && !message.forwarded_from && typeof(message.text)==='string'){
+                    names_posts[message.from].ownText?
+                        names_posts[message.from].ownText++ :
+                        names_posts[message.from].ownText=1;
+                }
+                if(message.forwarded_from){
+                    names_posts[message.from].fwded?
+                        names_posts[message.from].fwded++ :
+                        names_posts[message.from].fwded=1;
+                }
+            }
+        })
+        //массив из пар К-З и его сортировка по total числу сообщений :
+        let ascen = Object.entries(names_posts).sort
+                        (function (nc1,nc2){
+                            return nc2[1].total-nc1[1].total
+                        });
+        //новый объект из уже отсортированного массива
+        names_posts = Object.fromEntries(ascen);
+
+        Object.keys(names_posts).forEach(author => {
+            personal(author)
+        });
+        console.table(names_posts); //Объект вида {Автор: {total:567, ownText:123, fwded:43, allWords:{'не':34}}}
+
+        console.log(names_posts['Понтелей Мтс'])
+        // setTimeout(()=>{
+        //     personal('Понтелей Мтс');
+        // },3000);
+
+        response.render('stats',{names_posts:names_posts})
+
+/*        fs.writeFileSync('./uploads/out.json',entrary);
+            console.log('2: file wrote to disk');
         fs.readFile('./uploads/out.json', (err, data) => {
             if (err) throw err;
             chatArray = JSON.parse(data.toString()).messages;
@@ -109,8 +155,6 @@ exP.post('/sendFile',(request,response)=>{
             //     personal('Понтелей Мтс');
             // },3000);
             response.render('stats',{x:names_posts})
-        });
+        });*/
     });
-
-    //request.pipe(fs.createWriteStream('./downloaded/out.json')); //файл создаётся, но криво (лишние строки)
 })
